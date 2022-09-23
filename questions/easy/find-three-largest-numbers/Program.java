8460346ba612837747f7340c0ee7114e0386e355
Oscar Bacilio <baciliooscar@gmail.com>
2022-09-22 19:22:54 -0500

    Add easy find-three-largest-numbers

---
+++ questions/easy/find-three-largest-numbers/Program.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Program {
+  public static int[] findThreeLargestNumbers(int[] array) {
+	int[] solutions=new int[]{Integer.MIN_VALUE,Integer.MIN_VALUE,Integer.MIN_VALUE};
+	for(int val:array){
+		if(val<=solutions[0]){continue;}
+		if(val>solutions[0] && val<=solutions[1]){
+			solutions[0]=val;
+		}
+		else if(val>solutions[1] && val<=solutions[2]){
+			solutions[0]=solutions[1];
+			solutions[1]=val;
+		}
+		else if(val>solutions[2]){
+			solutions[0]=solutions[1];
+			solutions[1]=solutions[2];
+			solutions[2]=val;
+		}
+	}
+    return solutions;
+  }
+}
