3da9ff014b43d496c5d48ddd861e5b1d2a0b851e
Oscar Bacilio <baciliooscar@gmail.com>
2022-09-24 00:00:52 -0500

    Add easy two-number-sum

---
+++ questions/easy/two-number-sum/Program.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+		Set<Integer> numbers = new HashSet<Integer>();
+		for(int val:array){
+			if(numbers.contains(targetSum-val)){
+				return new int[]{val,targetSum-val};
+			}else{
+				numbers.add(val);
+			}
+		}
+    return new int[0];
+  }
+}
