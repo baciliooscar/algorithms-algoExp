e0c62c1f70294395b2ef36e04e8579db00b7b575
Oscar Bacilio <baciliooscar@gmail.com>
2022-10-02 23:39:38 -0500

    Add easy solution for depth-first-search

---
+++ questions/easy/depth-first-search/Program.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Program {
+  // Do not edit the class below except
+  // for the depthFirstSearch method.
+  // Feel free to add new properties
+  // and methods to the class.
+  static class Node {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public List<String> depthFirstSearch(List<String> array) {
+			array.add(this.name);
+			for(Node n:this.children){
+				n.depthFirstSearch(array);
+			}
+			
+      return array;
+    }
+
+    public List<String> solution(List<String> array) {
+			array.add(this.name);
+			System.out.println(this.name);
+			System.out.println(this.children.size());
+			
+      return array;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+  }
+}
